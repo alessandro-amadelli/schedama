c34179d04c01e5bf1bdd0d6206438de2e50f9e9b
alessandro-amadelli <alessandro.amadelli@gmail.com>
2025-07-31 11:01:39 +0200

    removed django-compressor temporary because the push like...ehm...broke everything

--- schedama/settings.py
+++ schedama/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'compressor',
     # 'pwa',
 ]
 
@@ -54,12 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
 ]
 
 ROOT_URLCONF = 'schedama.urls'
